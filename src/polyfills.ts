66803e5cb494ff2b6f6162690df06fe47f6ecadf
jacquihaugheypeterahnwedding <jacquihaugheypeterahnwedding@gmail.com>
2023-10-18 18:22:29 -0400

    add auth

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -2,3 +2,7 @@
 (window as any).process = {
   env: { DEBUG: undefined },
 };
+
+import 'zone.js/dist/zone'; // Included with Angular CLI.
+
+(window as any).Zone['__zone_symbol__ignoreConsoleErrorUncaughtError'] = true;
