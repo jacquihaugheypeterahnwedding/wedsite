8f7106aa96998e433e0aefbf138d42052039fa54
jacquihaugheypeterahnwedding <jacquihaugheypeterahnwedding@gmail.com>
2023-10-18 18:13:37 -0400

    add restraunt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AmplifyAuthenticatorModule } from '@aws-amplify/ui-angular';
+
+/* new form imports */
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RestaurantsComponent } from './restaurants/restaurants.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, RestaurantsComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    /* configure App with AmplifyAuthenticatorModule */
-    AmplifyAuthenticatorModule
+    AmplifyAuthenticatorModule,
+    /* configuring form modules */
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
