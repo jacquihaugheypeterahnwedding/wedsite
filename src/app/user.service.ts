3ffe0311eeaf77ea6a702228a0ee58c42cf6596a
jacquihaugheypeterahnwedding <jacquihaugheypeterahnwedding@gmail.com>
2024-02-12 13:35:14 -0500

    fix rsvp bug

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -5,6 +5,7 @@ import { APIService, ListUserSettingsQuery, UserSettings, User, ListUsersQuery, 
 import { CognitoService } from './cognito.service';
 import { I18n } from 'aws-amplify';
 import { Observable, ReplaySubject } from 'rxjs';
+import { Hub } from 'aws-amplify';
 
 @Injectable({
   providedIn: 'root',
@@ -24,8 +25,45 @@ export class UserService {
   constructor(private api: APIService, private cognitoService: CognitoService) {
 
     this.cognitoService.getUser().then(value => {
+      console.log('cognito');
+      console.log(value);
+      if (value) {
         this.username = value.username;
+        this.start();
+      }
+        
 
+
+    });
+
+
+
+    const listener = (data) => {
+      switch (data?.payload?.event) {
+        case 'signIn':
+          console.log('user signed in');
+          console.log(data.payload.data.username);
+          this.username = data.payload.data.username;
+          this.start();
+          break;
+        
+        default:
+          console.log('unknown event type');
+          break;
+      }
+    };
+
+    Hub.listen('auth', listener);
+
+
+    
+  }
+
+
+
+  start() {
+    
+    console.log('statr here');
 
 
         const login: CreateLoginInput = {
@@ -53,17 +91,6 @@ export class UserService {
           }
           this.user_info_obs.next(null);
         });
-
-
-         
-        
-
-
-
-    });
-
-
-    
   }
 
 
